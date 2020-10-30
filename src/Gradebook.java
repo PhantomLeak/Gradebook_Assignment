413d2e36cedcc89e1295d914f003461fe3f87df5
PhantomLeak <dstocking0823@gmail.com>
2020-10-30 09:37:51 -0400

    Got the code looping, writing, and reading perfectly, just need to add in the math to do the grade calculations.

--- src/Gradebook.java
+++ src/Gradebook.java
@@ -1,53 +1,35 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Gradebook {
 
     public static void main(String [] args) throws IOException {
         Student student = new Student();
-
-        getStudentData(student);
-    }
-
-    public static int getStudentData(Student student) throws IOException {
         Scanner scan = new Scanner(System.in);
 
-        FileReader reader = new FileReader("src/StudentGrades.txt");
-        FileWriter writer = new FileWriter("src/StudentGrades.txt");
-
-        ArrayList<String> classOptions = new ArrayList<>();
-        classOptions.add("Blaster deflection");
-        classOptions.add("Lightsaber combat");
-        classOptions.add("Force control");
-
-        System.out.print("What is your name?: ");
+        System.out.print("What is your name?: ");  //Getting student name
         String studentName = scan.nextLine();
 
-        System.out.print("So " + studentName + " are you entering new grades or viewing old ones? [Enter new or old]: ");
-        String viewChoice = scan.nextLine();
+        System.out.print("So " + studentName + " are you entering new grades or viewing old ones? [Enter new or old]: ");  //determine is student is looking at old grades or entering new ones.
+        String viewChoice = scan.nextLine().toLowerCase();
 
-        if (viewChoice == "new") {
-            try (BufferedWriter gradeWriter = new BufferedWriter(writer)) {
-
-                System.out.print("So " + studentName + " What class are you entering grades for?: " + classOptions + ": "); //Student choosing what class they want to take...
-                String classDecision = scan.nextLine();
-                writer.write(classDecision);
-            } catch (IOException fileError) {
-                System.out.println("File Error: " + fileError.getStackTrace());
-            }
-            writer.close();
-        } else {
-            try (BufferedReader gradeReader = new BufferedReader(reader)) {
-                String line;
-                gradeReader.close();
-            } catch (FileNotFoundException ex) {
-                System.out.println("File does not exist: " + ex.getStackTrace());
-            } catch (IOException ex) {
-                System.out.println("Problem reading file: " + ex.getStackTrace());
-            }
-
+        switch (viewChoice) {
+            case "new":
+                student.newGradeInputs();
+                break;
+            case "old":
+                student.showOldGrades();
+                student.printOldGrades();
+                break;
+            default:
+                System.out.println("I don't understand");
         }
-        return 0;
+
+
     }
+
+
 }
--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,8 @@
+import java.io.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Student {
     private int exam = 100;
@@ -7,9 +10,79 @@ public class Student {
     private int homework = 250;
     private int project = 125;
     private int total = 700;
+    public List<String> grades = new ArrayList<>();
 
     public Student() {}
 
+
+    protected void newGradeInputs() throws IOException {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<String> classOptions = new ArrayList<>();
+        classOptions.add("Blaster deflection");
+        classOptions.add("Lightsaber combat");
+        classOptions.add("Force control");
+        File txtFile = new File("src/StudentGrades.txt");
+
+        if(!txtFile.exists()) {
+            txtFile.createNewFile();
+        }
+
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter(txtFile, true));
+
+            System.out.print( "What class are you entering grades for?: " + classOptions + ": "); //Student choosing what class they want to take...
+            String classDecision = scan.nextLine();
+
+            System.out.print("What types of grades are you entering? [Exam, Quiz, Project, Homework]: ");
+            String gradeTypeDecision = scan.nextLine();
+
+            System.out.print("Please enter the grade you received for your " + gradeTypeDecision + " : ");
+
+            writer.write(classDecision + " " + "[" + gradeTypeDecision + "]" + "\n");
+            writer.close();
+
+            System.out.print("Would you like to enter any other grades? [yes or no]: ");
+            String moreGradeInputs = scan.nextLine().toLowerCase();
+            if (moreGradeInputs.equals("yes")){  //create loop that lets user input more than one grade
+                newGradeInputs();
+            }else {
+                writer.close();
+            }
+
+        } catch(IOException writterEX){
+            System.out.println("Cannot Write to file: permission issue: " + writterEX.getStackTrace());
+
+        }
+    }
+
+    protected void showOldGrades() {
+        try {
+            FileReader reader = new FileReader("src/StudentGrades.txt"); //Makes files readable
+            BufferedReader bReader = new BufferedReader(reader);  //Reads it one line at a time
+
+            String line = "";
+            while ((line = bReader.readLine()) != null) {
+                grades.add(line.toLowerCase());
+            }
+
+            reader.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("File does not exist: " + ex.getStackTrace());
+        } catch (IOException ex) {
+            System.out.println("Problem reading file: " + ex.getStackTrace());
+        }
+    }
+
+    protected void printOldGrades() {
+        if(grades.size() < 1) {
+            System.out.println("Grades not loaded");
+        } else{
+            System.out.println("Your old grades are : ");
+            for(String i : grades) {
+                System.out.println(i);
+            }
+        }
+    }
 
 
 
