1927b5c6cf81d07d537ee7c914faec298ec2d5fe
PhantomLeak <dstocking0823@gmail.com>
2020-10-30 00:17:08 -0400

    Trying to clean up code and begin adding FileReader and Writer.

--- src/Gradebook.java
+++ src/Gradebook.java
@@ -1,9 +1,31 @@
-
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Gradebook {
-    public static void main(String args[]) {
-        Student student = new Student();
-        System.out.println(student.studentInfo());
 
-        }
+    public static void main(String [] args) {
+        Student student = new Student();
+
+        getStudentData(student);
+    }
+
+    public static int getStudentData(Student student) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("What is your name?: ");
+        String studentName = scan.nextLine();
+
+        ArrayList<String> classOptions = new ArrayList<>();
+        classOptions.add("Blaster deflection");
+        classOptions.add("Lightsaber combat");
+        classOptions.add("Force control");
+
+        System.out.print("So " + studentName + " What class are you entering grades for?: " + classOptions + ": "); //Student choosing what class they want to take...
+        String classDecision = scan.nextLine();
+
+        System.out.print(classDecision + " A very fun class, what type of grade are you entering? [Exam, Homework, Quiz, Project]: ");
+        String gradeType = scan.nextLine();
+
+        return 0;
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,58 +1,19 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Student {
+    private int exam = 100;
+    private int quizzes = 225;
+    private int homework = 250;
+    private int project = 125;
+    private int total = 700;
 
-    public static String studentInfo(){
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Please enter your name: ");
-        String studentName = scan.nextLine();
-
-        ArrayList<String> classOptions = new ArrayList<>();
-        classOptions.add("Lightsaber training");
-        classOptions.add("Blaster deflection training");
-        classOptions.add("Force control training");
-
-        System.out.println(classOptions);
-        System.out.print("Please select your class: ");  //Gives student the choices they can choose from...
-        String classChoice = scan.nextLine();
-
-        System.out.print("What type of Grade are you inputting for " + classChoice + " (Exam, Quiz, Homework, Project) : ");
-        String typeOfGrade = scan.nextLine();
-
-        switch (typeOfGrade) {
-            case "Exam":
-                examCalc();
-                break;
-            case "Quiz":
-                System.out.print("Please enter the grade you got for the Quiz: ");
-                //Insert calculations for Quiz
-                break;
-            case "Project":
-                System.out.print("Please enter the grade you got for the Project: ");
-                //Insert calculations for Project
-            case "Homework":
-                System.out.print("Please enter the grade you got for the Homework: ");
-                //Insert calculations for Homework
-            default:
-                System.out.println("There was an error...Let's try that again");
-                studentInfo();
-        }
-
-        return studentName;  //Using as a holder so the method can simply work instead of giving me an error...
-    }
+    public Student() {}
 
 
-    protected static int examCalc() {
-        Scanner scan = new Scanner(System.in);
-        int Exam = 100;
-        System.out.print("Please enter the grade you got for the exam: ");
-        int examGrade = scan.nextInt();
 
-        switch (examGrade){
-            case 90-100:
-                System.out.println("You got an A!");
-        }
-        return examGrade;
-    }
+
+
+
+
 }
