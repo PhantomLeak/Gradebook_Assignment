0654e97ad2ca775bec8445eaa17fede761242ba7
PhantomLeak <dstocking0823@gmail.com>
2020-11-02 23:10:17 -0500

    Got the code looping, writing, and reading perfectly, just need to add in the math to do the grade calculations... Still haven't gotten the math to work properly though.

--- src/Student.java
+++ src/Student.java
@@ -1,8 +1,5 @@
 import java.io.*;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Student {
     private int exam = 100;
@@ -11,9 +8,19 @@ public class Student {
     private int project = 125;
     private int total = 700;
     public List<String> grades = new ArrayList<>();
+    public List<Integer> storeInfo = new ArrayList<>();
 
     public Student() {}
+    int examGrade;
+    int projectGrade;
+    int homeworkGrade;
+    int quizGrade;
 
+    int examTotal;
+    int quizTotal;
+    int projectTotal;
+    int homeworkTotal;
+    double finalGrade;
 
     protected void newGradeInputs() throws IOException {
         Scanner scan = new Scanner(System.in);
@@ -27,33 +34,122 @@ public class Student {
             txtFile.createNewFile();
         }
 
-        try{
+        try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(txtFile, true));
 
-            System.out.print( "What class are you entering grades for?: " + classOptions + ": "); //Student choosing what class they want to take...
+            System.out.print("What class are you entering grades for?: " + classOptions + ": "); //Student choosing what class they want to take...
             String classDecision = scan.nextLine();
 
             System.out.print("What types of grades are you entering? [Exam, Quiz, Project, Homework]: ");
-            String gradeTypeDecision = scan.nextLine();
+            String gradeTypeDecision = scan.nextLine().toLowerCase();
 
-            System.out.print("Please enter the grade you received for your " + gradeTypeDecision + " : ");
+            switch (gradeTypeDecision) {
+                case "exam":
+                    examCalculation();
+                    break;
+                case "quiz":
+                    quizCalculation();
+                    break;
+                case "project":
+                    projectCalculation();
+                    break;
+                case "homework":
+                    homeworkCalculation();
+                    break;
+                default:
+                    System.out.println("Error, please try again");
+                    newGradeInputs();
+                    break;
+            }
 
-            writer.write(classDecision + " " + "[" + gradeTypeDecision + "]" + "\n");
-            writer.close();
+            //Equation to add up all inputted grades.
+            int sumOfGrades = 0;
+            for (int i=0; i<storeInfo.size(); i++) {
+                sumOfGrades += storeInfo.get(i);
+
+            }
 
             System.out.print("Would you like to enter any other grades? [yes or no]: ");
             String moreGradeInputs = scan.nextLine().toLowerCase();
-            if (moreGradeInputs.equals("yes")){  //create loop that lets user input more than one grade
+            if (moreGradeInputs.equals("yes")) {  //create loop that lets user input more than one grade
+                writer.write("You got a " + storeInfo + " on your " + gradeTypeDecision + " in your " + classDecision + "\n");
+                writer.close();
                 newGradeInputs();
-            }else {
+            } else {
+                writer.write("You got a " + storeInfo + " on your " + gradeTypeDecision + " in your " + classDecision + "\n");
+                writer.write("Your overall grade for the class is a " + sumOfGrades);
                 writer.close();
             }
 
-        } catch(IOException writterEX){
-            System.out.println("Cannot Write to file: permission issue: " + writterEX.getStackTrace());
+        } catch(IOException writerEX){
+            System.out.println("Cannot Write to file: permission issue: " + writerEX.getStackTrace());
 
         }
     }
+
+    protected double examCalculation(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter the grade you received: ");
+        String gradeExamInput = scan.nextLine();
+
+        if (gradeExamInput.length() > 0){
+            examGrade = Integer.parseInt(gradeExamInput);
+            examTotal = ((examGrade*30)/30);
+            storeInfo.add(examTotal);
+        }else{
+            examTotal = 0;
+        }
+
+        return examTotal;
+    }
+
+    protected double quizCalculation(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter the grade you received: ");
+        String gradeQuizInput = scan.nextLine();
+        quizGrade = Integer.parseInt(gradeQuizInput);
+        if (quizGrade > 0){
+            quizTotal = ((quizGrade*20)/20);
+            storeInfo.add(quizTotal);
+        }else{
+            quizTotal = 0;
+        }
+
+        return quizTotal;
+    }
+
+    protected double homeworkCalculation(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter the grade you received: ");
+        String gradehomeworkInput = scan.nextLine();
+
+        if (gradehomeworkInput.length() > 0) {
+            homeworkGrade = Integer.parseInt(gradehomeworkInput);
+            homeworkTotal = ((homeworkGrade*25)/25);
+            storeInfo.add(homeworkTotal);
+        }else{
+            homeworkTotal = 0;
+        }
+
+        return homeworkTotal;
+    }
+
+    protected double projectCalculation(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter the grade you received: ");
+        String gradeprojectInput = scan.nextLine();
+
+        if (gradeprojectInput.length() > 0) {
+            projectGrade = Integer.parseInt(gradeprojectInput);
+            projectTotal = ((projectGrade*25)/25);
+            storeInfo.add(homeworkTotal);
+        }else{
+            projectTotal = 0;
+        }
+
+        return projectTotal;
+    }
+
 
     protected void showOldGrades() {
         try {
